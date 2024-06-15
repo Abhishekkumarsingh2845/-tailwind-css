8b21370cbd675ebbf02c4dff0398c0140d2c177a
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-06-15 18:54:42 +0530

    login page done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Component/Header";
 import NavBar from "./Component/NavBar";
 import Footer from "./Component/Footer";
+import Login from "./Screen/Login";
 
 export default function App() {
   return (
     <div className="flex flex-col ">
-     {/* <Header/>
-     <NavBar/> */}
+     <Header/>
+     <NavBar/>
+     <Login/>
      <Footer/>
     </div>
   );
